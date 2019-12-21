34158cdeacb1f4eb125f24a058bdfcac3d29cbfa
Jacob Wiltse <aquabug@gmail.com>
2019-12-21 16:33:50 -0500

    update tests

--- rcon_test.go
+++ rcon_test.go
@@ -2,25 +2,35 @@ package mc_rcon
 
 import "testing"
 
-func TestOpen(t *testing.T) {
-	conn := new(MCConn)
+var conn *MCConn
+
+func TestMCConn_Open(t *testing.T) {
+	conn = new(MCConn)
 	err := conn.Open("localhost:25575", "testpw")
 	if err != nil {
 		t.Error("Open failed", err)
 		return
 	}
-	defer conn.Close()
+}
 
-	err = conn.Authenticate()
+func TestMCConn_Authenticate(t *testing.T) {
+	err := conn.Authenticate()
 	if err != nil {
 		t.Error("Auth failed", err)
 		return
 	}
+}
 
-	resp, err := conn.SendCommand("tps")
+func TestMCConn_SendCommand(t *testing.T) {
+	resp, err := conn.SendCommand("seed")
 	if err != nil {
 		t.Error("Command failed", err)
+		return
 	}
 
 	t.Log(resp)
+}
+
+func TestMCConn_Close(t *testing.T) {
+	conn.Close()
 }
